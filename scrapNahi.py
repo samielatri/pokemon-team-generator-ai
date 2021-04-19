c88e3cfb4159cd01e0f11c5ffbbbdb3c2660336a
nahidath <vatoch@hotmail.fr>
2021-04-19 17:29:00 -0400

    dataframe 1ere version

--- scrapNahi.py
+++ scrapNahi.py
@@ -2,6 +2,7 @@ from bs4 import BeautifulSoup
 import urllib.request
 import re
 import pandas as pd
+
 # here we have to pass url and path
 # (where you want to save ur text file)
 url = 'https://www.smogon.com/stats/2021-02/moveset/gen8uu-0.txt'
@@ -9,64 +10,150 @@ print('Grabbing the page...')
 page = urllib.request.urlopen(url)
 soup = BeautifulSoup(page, 'html.parser')
 
-
-
-file = open("test2.txt", "w")
-print("start for")
+# first we do the scrapping
+file = open("scrapped.txt", "w")
 for data in soup:
     file.write(str(data))
 file.close()
-print("1st file done")
-print("end for")
+print("scrapping done")
 
-df=pd.read_csv("test2.txt", engine='python',encoding='cp1252', sep= "|")
+# second we add the scrapping page into a dataframe
+data = pd.DataFrame(
+    columns=['Name', 'Raw count', 'Avg. weight', 'Viability Ceiling', 'Abilities', 'Items', 'Spreads', 'Moves',
+             'Teammates', 'Checks and Counters'])
 
-print(df)
-data= pd.DataFrame(columns= ['Name', 'Raw count', 'Avg. weight','Viability Ceiling', 'Abilities', 'Items','Spreads', 'Moves','Teammates', 'Checks and Counters'])
-file2= open("finaltest.txt", "w")
-pattern="\\|(.+?)\\|"
-noline="+----------------------------------------+"
-with open("test2.txt") as fo:
-    """for rec in fo:
-        delResult=re.search(pattern,rec)
-        dd=""
-        if delResult:
-            dd = delResult.group()
-        file2.write(str(dd))
-        file2.write("\n")
-    file2.close()
-fo.close()"""
+pattern = "\\|(.+?)\\|"
+noline = "+----------------------------------------+"
+with open("scrapped.txt", "r+") as fo:
+
     for rec in fo:
-        if noline in rec:
-            next(fo)
-            delResult = re.search(pattern, rec)
-            dd = ""
-            if delResult:
-                ddname = delResult.group(1)
-        else:
-            delResult = re.search(pattern, rec)
-            dd = ""
-            if delResult:
-                dd = delResult.group(1)
-                part_string=dd.partition(':')
-                if "Raw count" in part_string[0]:
-                    ddRaw=part_string[1]
-                elif "Avg. weight" in part_string[0]:
-                    ddAvg = part_string[1]
-                elif "Viability Ceiling" in part_string[0]:
-                    ddViab = part_string[1]
-        delResult = re.search(pattern, rec)
+        ddRaw = ""
+        ddAvg = ""
+        ddViab = ""
+        ddName = ""
+        ddAbi = []
+        ddIt = []
+        ddSprd = []
+        ddMov = []
+        ddTeam = []
+        ddCC = []
         dd = ""
+        tmp = ""
+
+        if rec.strip() == noline:
+            continue
+        delResult = re.search(pattern, rec)
+        if delResult:
+            ddName = delResult.group(1)
+            ddName = ddName.split()[0]
+            pass
+        next(fo)
+        tmp = next(fo)
+        delResult = re.search(pattern, tmp)
         if delResult:
             dd = delResult.group(1)
-            if "Abilities" in dd:
-                next(fo)
-                delResult = re.search(pattern, rec)
-                dd = ""
-                if delResult:
-                    dd = delResult.group(1)
-                    ddAbi=dd
-
-
+            if re.search("Raw count", dd):
+                ddRaw = dd.split(':')[1]
+                pass
+        tmp = next(fo)
+        delResult = re.search(pattern, tmp)
+        if delResult:
+            dd = delResult.group(1)
+            if re.search("Avg. weight", dd):
+                ddAvg = dd.split(':')[1]
+                pass
+        tmp = next(fo)
+        delResult = re.search(pattern, tmp)
+        if delResult:
+            dd = delResult.group(1)
+            if re.search("Viability Ceiling", dd):
+                ddViab = dd.split(':')[1]
+                pass
+        tmp = next(fo)
+        if tmp == noline:
+            pass
+        tmp = next(fo)
+        delResult = re.search(pattern, tmp)
+        if delResult:
+            dd = delResult.group(1)
+            if re.search("Abilities", dd):
+                pass
+                while dd != noline:
+                    dd = re.search(pattern, next(fo))
+                    try:
+                        dd = dd.group(1).strip()
+                        ddAbi.append(dd)
+                    except AttributeError:
+                        tmp = next(fo)
+                        break
+        delResult = re.search(pattern, tmp)
+        if delResult:
+            dd = delResult.group(1)
+            if re.search("Items", dd):
+                pass
+                while dd != noline:
+                    dd = re.search(pattern, next(fo))
+                    try:
+                        dd = dd.group(1).strip()
+                        ddIt.append(dd)
+                    except AttributeError:
+                        tmp = next(fo)
+                        break
+        delResult = re.search(pattern, tmp)
+        if delResult:
+            dd = delResult.group(1)
+            if re.search("Spread", dd):
+                pass
+                while dd != noline:
+                    dd = re.search(pattern, next(fo))
+                    try:
+                        dd = dd.group(1).strip()
+                        ddSprd.append(dd)
+                    except AttributeError:
+                        tmp = next(fo)
+                        break
+        delResult = re.search(pattern, tmp)
+        if delResult:
+            dd = delResult.group(1)
+            if re.search("Moves", dd):
+                pass
+                while dd != noline:
+                    dd = re.search(pattern, next(fo))
+                    try:
+                        dd = dd.group(1).strip()
+                        ddMov.append(dd)
+                    except AttributeError:
+                        tmp = next(fo)
+                        break
+        delResult = re.search(pattern, tmp)
+        if delResult:
+            dd = delResult.group(1)
+            if re.search("Teammates", dd):
+                pass
+                while dd != noline:
+                    dd = re.search(pattern, next(fo))
+                    try:
+                        dd = dd.group(1).strip()
+                        ddTeam.append(dd)
+                    except AttributeError:
+                        tmp = next(fo)
+                        break
+        delResult = re.search(pattern, tmp)
+        if delResult:
+            dd = delResult.group(1)
+            if re.search("Checks and Counters", dd):
+                pass
+                while dd != noline:
+                    dd = re.search(pattern, next(fo))
+                    try:
+                        dd = dd.group(1).strip()
+                        ddCC.append(dd)
+                    except AttributeError:
+                        break
+        data = data.append(
+            {'Name': ddName, 'Raw count': ddRaw, 'Avg. weight': ddAvg, 'Viability Ceiling': ddViab, 'Abilities': ddAbi,
+             'Items': ddIt, 'Spreads': ddSprd, 'Moves': ddMov, 'Teammates': ddTeam, 'Checks and Counters': ddCC},
+            ignore_index=True)
 fo.close()
-print("2nd file done")
+data.to_csv(r'./export_poke.csv', index=False, header=True)
+print(data)
